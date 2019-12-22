b8cb85c61583156d07a734197e1c6aae887ab6bf
Rich Stutfield <rich@stutfield.net>
2019-12-22 22:39:52 +0000

    Add "implementation" plugin

---
+++ implementation.js
@@ -0,0 +1,33 @@
+const fs = require("fs");
+const Path = require("path");
+const _ = require("lodash");
+
+
+const sourceCache = {};
+
+// eslint-disable-next-line jsdoc/require-jsdoc
+function getSource(filename,[start,end]) {
+  if (!sourceCache[filename]) {
+    sourceCache[filename] = fs.readFileSync(Path.resolve(filename));
+  }
+  if (sourceCache[filename]) {
+    return sourceCache[filename].toString().substring(start,end);
+  }
+  return null;
+}
+
+exports.handlers = {
+  newDoclet: function({doclet}) {
+    if (doclet.undocumented) { return; }
+    const meta = doclet.meta || {};
+    const {filename,range} = meta;
+    if (filename && range) {
+
+      const columnno = meta.columnno || 0;
+      const indent = _.range(0,columnno).map(()=>" ").join("");
+      const src = `${indent}${getSource(filename,range)}`;
+      
+      doclet.implementation = src;
+    }
+  }
+};
