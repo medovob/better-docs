64f42af87e9a92a8fd625236770a164dd04887ca
Rich Stutfield <rich@stutfield.net>
2020-01-15 11:27:59 +0000

    Fix implementation generator for non-root level modules

--- implementation.js
+++ implementation.js
@@ -6,12 +6,13 @@ const _ = require("lodash");
 const sourceCache = {};
 
 // eslint-disable-next-line jsdoc/require-jsdoc
-function getSource(filename,[start,end]) {
-  if (!sourceCache[filename]) {
-    sourceCache[filename] = fs.readFileSync(Path.resolve(filename));
+function getSource(path,filename,[start,end]) {
+  const fullPath = Path.resolve(path,filename);
+  if (!sourceCache[fullPath]) {
+    sourceCache[fullPath] = fs.readFileSync(fullPath);
   }
-  if (sourceCache[filename]) {
-    return sourceCache[filename].toString().substring(start,end);
+  if (sourceCache[fullPath]) {
+    return sourceCache[fullPath].toString().substring(start,end);
   }
   return null;
 }
@@ -20,12 +21,12 @@ exports.handlers = {
   newDoclet: function({doclet}) {
     if (doclet.undocumented || (doclet.kind !== "function" && doclet.kind !== "class")) { return; }
     const meta = doclet.meta || {};
-    const {filename,range} = meta;
+    const {filename,range,path} = meta;
     if (filename && range) {
 
       const columnno = meta.columnno || 0;
       const indent = _.range(0,columnno).map(()=>" ").join("");
-      const src = `${indent}${getSource(filename,range)}`;
+      const src = `${indent}${getSource(path,filename,range)}`;
       
       doclet.implementation = src;
     }
